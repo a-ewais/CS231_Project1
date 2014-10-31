6c347c2ed1747631821a724964253ea2b61a867c
a-ewais <aa3_10000@yahoo.com>
2014-10-31 13:40:25 +0200

    all i and j

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 ofstream output;
 string out = "out.txt";
+int j;
 
 struct reg{
 	long int num = 0;
@@ -51,6 +52,7 @@ void fillregsec()
 
 void decode(unsigned int instWord)
 {
+
 	unsigned int rd, rs, rt, func, shamt, imm, opcode;
 	unsigned int address;
 	static unsigned int pc = 0x00400000;
@@ -68,6 +70,127 @@ void decode(unsigned int instWord)
 	else if (opcode == 2 || opcode == 3)
 	{
 		//j type
+		address = instWord & 0x03ffffff;
+		address = address << 2;
+		address = address | (pc & 0xf0000000);
+		if (opcode == 3)
+		{
+			output << "0x" << hex << pc << " jal " << "0x" << hex << address << endl;
+		}
+		else if (opcode == 2)
+		{
+
+			output << "0x" << hex << pc << " j " << "0x" << hex << address << endl;
+		}
+	}
+	else if (0 != opcode && 2 != opcode && 3 != opcode && 16 != opcode && 17 != opcode && 18 != opcode && 19 != opcode)
+	{
+		//I-type
+
+		rt = (instWord >> 16) & 0x1f;
+		rs = (instWord >> 21) & 0x1f;
+		imm = (instWord & 0x0000FFFF);
+
+
+		if (opcode == 8)
+		{
+			output << "0x" << hex << pc << " addi " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+		}
+		else if (opcode == 9)
+		{
+			output << "0x" << hex << pc << " addiu " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+		}
+		else if (opcode == 12)
+		{
+			output << "0x" << hex << pc << " andi " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+		}
+		else if (opcode == 13)
+		{
+			output << "0x" << hex << pc << " ori " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+		}
+		else if (opcode == 14)
+		{
+			output << "0x" << hex << pc << " xori " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+		}
+		else if (opcode == 4)
+		{
+			output << "0x" << hex << pc << " beq " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+		}
+		else if (opcode == 5)
+		{
+			output << "0x" << hex << pc << " bne " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+		}
+		else if (opcode == 10)
+		{
+			output << "0x" << hex << pc << " slt " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+		}
+		else if (opcode == 15)
+		{
+			output << "0x" << hex << pc << " lui " << regs[rt].secname << "," << imm << endl;
+		}
+		else if (opcode == 43)
+		{
+			output << "0x" << hex << pc << " sw " << regs[rt].secname << "," << imm << "(" << regs[rs].secname << ")" << endl;
+		}
+
+		else if (opcode == 35)
+		{
+			output << "0x" << hex << pc << " lw " << regs[rt].secname << "," << imm << "(" << regs[rs].secname << ")" << endl;
+		}
+		else if (opcode == 32)
+		{
+			output << "0x" << hex << pc << " lb " << regs[rt].secname << "," << imm << "(" << regs[rs].secname << ")" << endl;
+		}
+		else if (opcode == 40)
+		{
+			output << "0x" << hex << pc << " sb " << regs[rt].secname << "," << imm << "(" << regs[rs].secname << ")" << endl;
+		}
+		else if (opcode == 41)
+		{
+			output << "0x" << hex << pc << " sh " << regs[rt].secname << "," << imm << "(" << regs[rs].secname << ")" << endl;
+		}
+		else if (opcode == 33)
+		{
+			output << "0x" << hex << pc << " lh " << regs[rt].secname << "," << imm << "(" << regs[rs].secname << ")" << endl;
+		}
+
+	}
+
+	else
+	{
+		// The opcode is not known
+		cout << "0x" << hex << pc << " - Unkown Instruction" << endl;
+	}
+
+	pc += 4;
+}
+void debug(unsigned int instWord)
+{
+	unsigned int rd, rs, rt, func, shamt, imm, opcode;
+	unsigned int address;
+	static unsigned int pc = 0x00400000;
+	opcode = instWord >> 26;
+	if (opcode == 0)
+	{
+		//R-type
+		func = instWord & 0x3F;
+		shamt = (instWord >> 6) & 0x1f;
+		rd = (instWord >> 11) & 0x1f;
+		rt = (instWord >> 16) & 0x1f;
+		rs = (instWord >> 21) & 0x1f;
+
+	}
+	else if (opcode == 2 || opcode == 3)
+	{
+		//j type
+		address = instWord & 0x03ffffff;
+		address = address << 2;
+		address = address | (pc & 0xf0000000);
+		j = (pc - 0x00400000) / 4;
+		if (opcode == 3)
+			regs[31].num = pc + 4;
+		pc = address;
+		j = (pc - 0x00400000) / 4;
 	}
 	else
 	{
@@ -79,76 +202,146 @@ void decode(unsigned int instWord)
 
 
 		if (opcode == 8)
-		{
+		{//addi
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
 			regs[rt].num = regs[rs].num + imm;
-			output << "addi " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+
 		}
 		else if (opcode == 9)
-		{
+		{//addiu
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
 			regs[rt].num = regs[rs].num + imm;
-			output << "addiu " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
 		}
 		else if (opcode == 12)
-		{
+		{//andi
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
 			regs[rt].num = regs[rs].num & imm;
-			output << "andi " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
 		}
 		else if (opcode == 13)
-		{
+		{//ori
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
 			regs[rt].num = regs[rs].num | imm;
-			output << "ori " << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+		}
+		else if (opcode == 14)
+		{//xori
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
+			regs[rt].num = regs[rs].num ^ imm;
 		}
 		else if (opcode == 4)
-		{
+		{//beq
+
 			if (regs[rt].num == regs[rs].num)
 				pc += imm * 4;
-			output << "beq" << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
 		}
 		else if (opcode == 5)
-		{
+		{//bne
 			if (regs[rt].num != regs[rs].num)
 				pc += imm * 4;
-			output << "bne" << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
 		}
 		else if (opcode == 10)
-		{
+		{//slti
 			if (regs[rs].num < imm)
 				regs[rt].num = 1;
 			else
 				regs[rt].num = 0;
-			output << "slt" << regs[rt].secname << "," << regs[rs].secname << "," << imm << endl;
+
 		}
 		else if (opcode == 15)
-		{
+		{//lui
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
 			imm << 16;
 			regs[rt].num = regs[rt].num & 0x0000FFFF;
 			regs[rt].num = regs[rt].num | imm;
-			output << "lui" << regs[rt].secname << "," << imm << endl;
+
+		}
+		else if (opcode == 43)
+		{//sw
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
+			address = regs[rs].num + imm * 4;
+			memory[address++ - 0x10010000] = regs[rt].num & 0x000000ff;
+			memory[address++ - 0x10010000] = (regs[rt].num & 0x0000ff00) >> 8;
+			memory[address++ - 0x10010000] = (regs[rt].num & 0x00ff0000) >> 16;
+			memory[address++ - 0x10010000] = (regs[rt].num & 0xff000000) >> 24;
 		}
 
+		else if (opcode == 35)
+		{//lw
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
+			address = regs[rs].num + imm * 4;
+			regs[rt].num = 0;
+			regs[rt].num |= (memory[address-- - 0x10010000] << 24);
+			regs[rt].num |= (memory[address-- - 0x10010000] << 16);
+			regs[rt].num |= (memory[address-- - 0x10010000] << 8);
+			regs[rt].num |= memory[address - 0x10010000];
 
-		pc += 4;
+		}
+		else if (opcode == 32)
+		{//lb
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
+			address = regs[rs].num + imm;
+			regs[rt].num = memory[address - 0x10010000];
+
+		}
+		else if (opcode == 40)
+		{//sb
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
+			address = regs[rs].num + imm;
+			memory[address++ - 0x10010000] = regs[rt].num & 0x000000ff;
+
+		}
+		else if (opcode == 41)
+		{//sh
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
+			address = regs[rs].num + imm * 2;
+			memory[address++ - 0x10010000] = regs[rt].num & 0x000000ff;
+			memory[address++ - 0x10010000] = (regs[rt].num & 0x0000ff00) >> 8;
+
+		}
+		else if (opcode == 33)
+		{//lh
+			if (imm & 0x8000)
+				imm = imm & 0xffff0000;
+			address = regs[rs].num + imm * 4;
+			regs[rt].num = 0;
+			regs[rt].num |= (memory[address-- - 0x10010000] << 8);
+			regs[rt].num |= memory[address - 0x10010000];
+		}
 	}
+	pc += 4;
 }
-
 void main()
 {
 	ifstream infile;
 	string in = "sss.bin";
-	output.open(out.c_str());
-	unsigned int instruction;
+	unsigned int instruction[1024];
 	fillregsec();
 	infile.open(in.c_str(), ios::out | ios::binary);
 	if (infile.is_open())
 	{
-		while (infile.read((char*)&instruction, 4))
+		int i = 0;
+		output.open(out.c_str());
+		while (infile.read((char*)&instruction[i], 4))
 		{
-			decode(instruction);
+			decode(instruction[i++]);
+		}
+		output.close();
+		for (j = 0; j < i; j++)
+		{
+			debug(instruction[j]);
 		}
 	}
-
 	infile.close();
-	output.close();
 	cout << endl;
 	system("pause");
 }
